ff934c7277077c2bc7a034e578bda2a3c1edac82
Ruhul Amin <ruhulamin.cse56@gmail.com>
2026-03-01 00:18:03 +0600

    clean prisma

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,16 +1,5 @@
 import { Prisma } from '../../../generated/prisma';
 
-export class TempUser implements Prisma.TempUserCreateInput {
-  id: string;
-  email: string;
-  phoneNumber: string;
-  password: string;
-  fullName: string;
-  address: string;
-  otp: string;
-  expireAt: string | Date;
-}
-
 export class User implements Prisma.UserCreateInput {
   id: string;
   email: string;
